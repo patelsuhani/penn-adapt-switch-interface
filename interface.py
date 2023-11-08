58515aa49f36679c864df9a826ceaf40e15c50c9
srikargudimella <111258947+srikargudimella@users.noreply.github.com>
2023-11-07 19:06:12 -0500

    Simplify digital input pin logic and add analog switch pin logic
    This commit does the following:
    - Cleaned up the digital input pin logic to remove the pin arrays since we're using only one digital input pin
    - Added logic to check the value of analog input pin for switch. It then sets the output key signal accordingly
    
    Co-authored-by: Suhani Patel <patelsuhani07@gmail.com>

--- interface.py
+++ interface.py
@@ -9,32 +9,37 @@
 import time
 import board
 import digitalio
+import analogio
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.mouse import Mouse
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 
-# The pins we'll use, each will have an internal pullup
-keypress_pins = [board.ROTA, board.SWITCH]
-# Our array of key objects
-key_pin_array = []
+# Initialize the constants for voltage options of switch
+
+ENTER_VOLTAGE = 1
+SPACE_VOLTAGE = 2
+CLICK_VOLTAGE = 3
+
+SWITCH_TOLERANCE = 0.2
+# The make the SWITCH pin a digital input pin with internal pullup
+
+digital_button_pin = digitalio.DigitalInOut(board.SWITCH)
+digital_button_pin.direction = digitalio.Direction.INPUT
+digital_button_pin.pull = digitalio.Pull.UP
+
 # The Keycode sent for each button
 # To view a complete list of available keycodes: 
 # https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/keycode.html
 keys_pressed = [keycode.ENTER, keycode.SPACE, Mouse.LEFT_BUTTON]
 
+# Analog pin that's used for the toggle switch
+analog_switch_pin = analogio.AnalogIn(board.ROTA)
 # The keyboard object!
 time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
 keyboard = Keyboard(usb_hid.devices)
 keyboard_layout = KeyboardLayoutUS(keyboard)  # We're in the US :)
-
-# Make all pin objects inputs with pullups
-for pin in keypress_pins:
-    key_pin = digitalio.DigitalInOut(pin)
-    key_pin.direction = digitalio.Direction.INPUT
-    key_pin.pull = digitalio.Pull.UP
-    key_pin_array.append(key_pin)
 
 # Creates an instance of the on board light and sets it as an output
 led = digitalio.DigitalInOut(board.NEOPIXEL)
@@ -43,22 +48,37 @@ led.direction = digitalio.Direction.OUTPUT
 print("Waiting for key pin...")
 
 while True:
+    # Check analog switch Value
+    switch_val = analog_switch_pin.value
+
+    # If switch value = option 1 -> key_out = ENTER
+    if switch_val >= ENTER_VOLTAGE - SWITCH_TOLERANCE and switch_val <= ENTER_VOLTAGE + SWITCH_TOLERANCE:
+        key_out = Keycode.ENTER
+
+    # If switch value = option 2 -> key_out = SPACE
+    elif switch_val >= SPACE_VOLTAGE - SWITCH_TOLERANCE and switch_val <= SPACE_VOLTAGE + SWITCH_TOLERANCE:
+        key_out = Keycode.SPACE
+
+    # If switch value = option 3 -> key_out = LEFT_BUTTON
+    elif switch_val >= CLICK_VOLTAGE - SWITCH_TOLERANCE and switch_val <= CLICK_VOLTAGE + SWITCH_TOLERANCE:
+        key_out = Mouse.LEFT_BUTTON
+    
+    else:
+        key_out = Keycode.ENTER
+    
     # Check each pin
-    for key_pin in key_pin_array:
-        if not key_pin.value:  # Is it grounded?
-            i = key_pin_array.index(key_pin)
-            print("Pin #%d is grounded." % i)
+    if not digital_button_pin.value:  # Is it grounded?
+        
+        print("Pin switch is grounded.")
+        
+        # Turn on the red LED
+        led.value = True
 
-            # Turn on the red LED
-            led.value = True
-
-            while not key_pin.value:
-                pass  # Wait for it to be ungrounded!            
-            key = keys_pressed[i] # Get the keycode to press
-            keyboard.press(key)  # "Press"...
-            keyboard.release_all()  # ..."Release"!
-
-            # Turn off the red LED
-            led.value = False
+        while not digital_button_pin.value:
+            pass  # Wait for it to be ungrounded!            
+        keyboard.press(key_out)  # "Press"...
+        keyboard.release_all()  # ..."Release"!
+        # Turn off the red LED
+        led.value = False
 
     time.sleep(0.01)
