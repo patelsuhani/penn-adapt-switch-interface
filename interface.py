032bc616f36bd161e8967879491f5e5b90214903
srikargudimella <111258947+srikargudimella@users.noreply.github.com>
2023-11-10 17:31:15 -0500

    Change Switch input pin to Pull Down And Comment out Analog functions
    Since we haven't figured out how to get analogio library onto the board, we commented out.
    We also changed the SWITCH button input pin to a pulldown instead of pullup which we found to work with the button.
    
    Co-authored-by: Suhani Patel <patelsuhani07@gmail.com>

--- interface.py
+++ interface.py
@@ -9,7 +9,7 @@
 import time
 import board
 import digitalio
-import analogio
+# import analogio
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.mouse import Mouse
@@ -18,24 +18,26 @@ from adafruit_hid.keycode import Keycode
 
 # Initialize the constants for voltage options of switch
 
-ENTER_VOLTAGE = 1
-SPACE_VOLTAGE = 2
-CLICK_VOLTAGE = 3
+ENTER_VOLTAGE = 0.019
+SPACE_VOLTAGE = 1.6
+CLICK_VOLTAGE = 3.322
 
-SWITCH_TOLERANCE = 0.2
+SWITCH_TOLERANCE = 0.4
+
+# Set Power pin
+power_pin = digitalio.DigitalInOut(board.ROTB)
+power_pin.direction = digitalio.Direction.OUTPUT
+power_pin.value = True
+
 # The make the SWITCH pin a digital input pin with internal pullup
 
 digital_button_pin = digitalio.DigitalInOut(board.SWITCH)
 digital_button_pin.direction = digitalio.Direction.INPUT
-digital_button_pin.pull = digitalio.Pull.UP
+digital_button_pin.pull = digitalio.Pull.DOWN
 
-# The Keycode sent for each button
-# To view a complete list of available keycodes: 
-# https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/keycode.html
-keys_pressed = [keycode.ENTER, keycode.SPACE, Mouse.LEFT_BUTTON]
 
 # Analog pin that's used for the toggle switch
-analog_switch_pin = analogio.AnalogIn(board.ROTA)
+# analog_switch_pin = analogio.AnalogIn(board.ROTA)
 # The keyboard object!
 time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
 keyboard = Keyboard(usb_hid.devices)
@@ -49,32 +51,32 @@ print("Waiting for key pin...")
 
 while True:
     # Check analog switch Value
-    switch_val = analog_switch_pin.value
+    # switch_val = analog_switch_pin.value
 
-    # If switch value = option 1 -> key_out = ENTER
-    if switch_val >= ENTER_VOLTAGE - SWITCH_TOLERANCE and switch_val <= ENTER_VOLTAGE + SWITCH_TOLERANCE:
-        key_out = Keycode.ENTER
+    # # If switch value = option 1 -> key_out = ENTER
+    # if switch_val >= ENTER_VOLTAGE - SWITCH_TOLERANCE and switch_val <= ENTER_VOLTAGE + SWITCH_TOLERANCE:
+    #     key_out = Keycode.ENTER
 
-    # If switch value = option 2 -> key_out = SPACE
-    elif switch_val >= SPACE_VOLTAGE - SWITCH_TOLERANCE and switch_val <= SPACE_VOLTAGE + SWITCH_TOLERANCE:
-        key_out = Keycode.SPACE
+    # # If switch value = option 2 -> key_out = SPACE
+    # elif switch_val >= SPACE_VOLTAGE - SWITCH_TOLERANCE and switch_val <= SPACE_VOLTAGE + SWITCH_TOLERANCE:
+    #     key_out = Keycode.SPACE
 
-    # If switch value = option 3 -> key_out = LEFT_BUTTON
-    elif switch_val >= CLICK_VOLTAGE - SWITCH_TOLERANCE and switch_val <= CLICK_VOLTAGE + SWITCH_TOLERANCE:
-        key_out = Mouse.LEFT_BUTTON
+    # # If switch value = option 3 -> key_out = LEFT_BUTTON
+    # elif switch_val >= CLICK_VOLTAGE - SWITCH_TOLERANCE and switch_val <= CLICK_VOLTAGE + SWITCH_TOLERANCE:
+    #     key_out = Mouse.LEFT_BUTTON
     
-    else:
-        key_out = Keycode.ENTER
-    
+    # else:
+    #     key_out = Keycode.SPACE
+    key_out = Keycode.SPACE
     # Check each pin
-    if not digital_button_pin.value:  # Is it grounded?
+    if digital_button_pin.value:  # Is it grounded?
         
         print("Pin switch is grounded.")
         
         # Turn on the red LED
         led.value = True
 
-        while not digital_button_pin.value:
+        while digital_button_pin.value:
             pass  # Wait for it to be ungrounded!            
         keyboard.press(key_out)  # "Press"...
         keyboard.release_all()  # ..."Release"!
